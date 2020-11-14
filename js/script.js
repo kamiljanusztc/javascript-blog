c23314cc9fef5cf892842baadf537728f3f577cc
Kamil Janusz <kamiljanusztc@gmail.com>
2020-11-14 18:08:32 +0100

    Add format document

--- js/script.js
+++ js/script.js
@@ -5,54 +5,54 @@ document.getElementById('test-button').addEventListener('click', function(){
   });
   */
 
- const titleClickHandler = function(event) {
+const titleClickHandler = function (event) {
     event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
     console.log(event);
-  
+
     /* [DONE] remove class 'active' from all article links  */
 
     const activeLinks = document.querySelectorAll('.titles a.active');
 
-    for(let activeLink of activeLinks) {
+    for (let activeLink of activeLinks) {
         activeLink.classList.remove('active');
     }
-  
+
     /* [IN PROGRESS] add class 'active' to the clicked link */
 
     clickedElement.classList.add('active');
 
     console.log('clickedElement:', clickedElement);
-  
+
     /* [DONE] remove class 'active' from all articles */
 
     const activeArticles = document.querySelectorAll('.posts .active');
 
-    for(let activeArticle of activeArticles) {
+    for (let activeArticle of activeArticles) {
         activeArticle.classList.remove('active');
     }
-  
+
     /* get 'href' attribute from the clicked link */
 
     const href = clickedElement.getAttribute('href');
 
     console.log(href);
-  
+
     /* find the correct article using the selector (value of 'href' attribute) */
 
     const targetArticle = document.querySelector('href');
 
     console.log(targetArticle);
-  
+
     /* add class 'active' to the correct article */
 
     targetArticle.classList.add('active');
 
-  }
-  
-  const links = document.querySelectorAll('.titles a');
-  
-  for(let link of links){
+}
+
+const links = document.querySelectorAll('.titles a');
+
+for (let link of links) {
     link.addEventListener('click', titleClickHandler);
-  }
+}
