5c88fdf93a63a9fb99b98b2546849327edb36bd0
Kamil Janusz <kamiljanusztc@gmail.com>
2020-11-23 21:19:37 +0100

    Change array to object in script.js

--- js/script.js
+++ js/script.js
@@ -96,8 +96,8 @@ document.getElementById('test-button').addEventListener('click', function(){
 
   const generateTags = function() {
 
-    /* [NEW] create a new variable allTags with an empty array */
-    let allTags = [];
+    /* [NEW] create a new variable allTags with an empty object */
+    let allTags = {};
 
     /* find all articles */
     const articles = document.querySelectorAll(optArticleSelector);
